2fc5d4aa6e71f4a990980c5e6d0ce8eaf48dff9c
Muhammad Amanullah Khan <amanullah.khan@hiltonpharma.com>
2022-07-05 04:03:04 -0700

    Amanullah - MoM_Production - Implemented file upload

--- src/NewMeetings/sessionsTable.jsx
+++ src/NewMeetings/sessionsTable.jsx
@@ -55,6 +55,11 @@ export default function DataGridDemo(props) {
   const [sure, setSure] = React.useState(false);
   const [selectedSessionID, setSelectedSessionID] = React.useState();
   const [disableAgendas, setDisableAgendas] = React.useState(false);
+  const [disableAttachments, setDisableAttachments] = React.useState(false);
+  const [attachments, setAttachments] = React.useState([])
+  const [formDatas, setFormDatas] = React.useState([])
+  const [attachmentUploaded, setAttachmentUploaded] = React.useState(false)
+  const [currentAttachment, setCurrentAttachment] = React.useState('')
   const history = useHistory();
 
   React.useEffect(() => {
@@ -68,6 +73,12 @@ export default function DataGridDemo(props) {
   React.useEffect(() => {
     if (!localStorage.getItem("meetingID")) {
       setDisableAgendas(true);
+    }
+  });
+
+  React.useEffect(() => {
+    if (!localStorage.getItem("meetingID")) {
+      setDisableAttachments(true);
     }
   });
 
@@ -448,6 +459,139 @@ export default function DataGridDemo(props) {
     }
   };
 
+  const saveAttachments = async () => {
+    const uploads = await axios.post(`${BASE_URL}/upload`, formDatas, {
+      userid: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")).id : '',
+      meetingId: ''
+    });
+
+    console.log(uploads)
+  }
+
+  const getFile = (event) => {
+    let files = attachments
+    let datas = formDatas
+
+    setCurrentAttachment(event.target.files[0])
+
+    if (event.target.files[0].type.includes('pdf') ||
+      event.target.files[0].type.includes('png') ||
+      event.target.files[0].type.includes('jpg') ||
+      event.target.files[0].type.includes('jpeg') ||
+      event.target.files[0].type.includes('zip') ||
+      event.target.files[0].type.includes('txt') ||
+      event.target.files[0].type.includes('text') ||
+      event.target.files[0].type.includes('doc') ||
+      event.target.files[0].type.includes('docx') ||
+      event.target.files[0].type.includes('xlsx') ||
+      event.target.files[0].type.includes('csv') ||
+      event.target.files[0].type.includes('ppt')) {
+
+      if (event.target.files[0].size > 1024 * 1024 * 5) {
+        console.log('large file')
+        setNotificationState(true)
+        setNotificationText('File too large')
+        setNotificationType('error')
+        setCurrentAttachment('')
+      } else {
+
+        const existing = attachments.filter((item) => {
+          return event.target.files[0].name == item.name
+        })
+
+        if (existing.length != 0) {
+          console.log('already there')
+          setNotificationState(true)
+          setNotificationText('File already uploaded')
+          setNotificationType('error')
+          setCurrentAttachment('')
+        } else {
+          const formData = new FormData();
+          formData.append(
+            "file",
+            event.target.files[0],
+          );
+
+          files.push(event.target.files[0])
+          datas.push(formData)
+          setAttachmentUploaded(!attachmentUploaded)
+          setAttachments(files)
+          setFormDatas(datas)
+          setCurrentAttachment('')
+          console.log(files)
+        }
+      }
+    } else {
+      console.log('rejected')
+      setNotificationState(true)
+      setNotificationText('Invalid file format')
+      setNotificationType('error')
+    }
+  }
+
+  const getFileIcon = (file) => {
+    let type = ''
+
+    if (file.name.includes('txt')) {
+      type = 'txt'
+    } else if (file.name.includes('jpg')) {
+      type = 'jpg'
+    } else if (file.name.includes('jpeg')) {
+      type = 'jpg'
+    } else if (file.name.includes('xlsx')) {
+      type = 'xls'
+    } else if (file.name.includes('csv')) {
+      type = 'csv'
+    } else if (file.name.includes('png')) {
+      type = 'png'
+    } else if (file.name.includes('pdf')) {
+      type = 'pdf'
+    } else if (file.name.includes('docx')) {
+      type = 'doc'
+    } else if (file.name.includes('doc')) {
+      type = 'doc'
+    } else if (file.name.includes('zip')) {
+      type = 'zip'
+    } else if (file.name.includes('ppt')) {
+      type = 'ppt'
+    }
+
+    return <img style={{ width: 30 }} src={require(`../FileIcons/${type}.png`).default} />
+  }
+
+  const removeAttachment = (file) => {
+    let index = attachments.indexOf(file)
+    let newAttachments = attachments
+
+    newAttachments.splice(index, 1)
+    setCurrentAttachment('')
+    setAttachments(newAttachments)
+    setAttachmentUploaded(!attachmentUploaded)
+  }
+
+  const getFileElement = (file) => {
+    return <div style={{ width: '95%', display: 'flex', padding: 5 }}>
+      <div style={{ width: "10%" }}>{getFileIcon(file)}</div>
+      <div style={{ width: "80%", paddingTop: 5 }}>{file.name}</div>
+      <div style={{ width: "10%", paddingTop: 2 }}><CancelIcon onClick={() => removeAttachment(file)} style={{ cursor: 'pointer' }} /></div>
+    </div>
+  }
+
+  const getAttachmentState = () => {
+    if (!disableAttachments && !attachments.length) {
+      //false and false
+      return true
+    } else if (!disableAttachments && attachments.length) {
+      //false and true
+      return true
+    } else if (disableAttachments && !attachments.length) {
+      //true and false
+      return true
+    } else {
+      return false
+    }
+  }
+
   return (
     <div>
       <div
@@ -458,47 +602,87 @@ export default function DataGridDemo(props) {
           display: "flex",
         }}
       >
-        <div style={{ width: "60%" }}>
-          <h3>Session Date</h3>
-          {/* <input
+        <div style={{ width: "70%", display: 'flex' }}>
+          <div style={{ width: "40%" }}>
+            <h3>Session Date</h3>
+            {/* <input
             style={{ padding: 10 }}
             type="date"
             value={sessionDate}
             //max={minDate}
             onChange={(data) => changePickDate(data)}
           /> */}
-          <DesktopDatePicker
-            value={sessionDate}
-            inputFormat="dd/MM/yyyy"
-            onChange={(data) => changePickDate(data)}
-            renderInput={(props) => <TextField {...props} />}
-          />
+            <DesktopDatePicker
+              value={sessionDate}
+              inputFormat="dd/MM/yyyy"
+              onChange={(data) => changePickDate(data)}
+              renderInput={(props) => <TextField {...props} />}
+            />
 
-          <br />
-          <Button
-            onClick={addSession}
-            style={{ marginTop: 20 }}
-            variant="contained"
-            disabled={dateChanged}
-          >
-            Add Session
-          </Button>
-          <Button
-            disabled={props.checkFields || disableSave}
-            onClick={saveMeeting}
-            style={{ marginTop: 20, marginLeft: 10, background: "green" }}
-            variant="contained"
-          >
-            Save
-          </Button>
+            <br />
+            <Button
+              onClick={addSession}
+              style={{ marginTop: 20 }}
+              variant="contained"
+              disabled={dateChanged}
+            >
+              Add Session
+            </Button>
+            <Button
+              disabled={props.checkFields || disableSave}
+              onClick={saveMeeting}
+              style={{ marginTop: 20, marginLeft: 10, background: "green" }}
+              variant="contained"
+            >
+              Save
+            </Button>
 
-          {!localStorage.getItem("meetingID") && (
-            <p style={{ fontSize: "12px" }}>
-              *Would be able to add agendas, once the meeting is saved.
-            </p>
-          )}
+            {!localStorage.getItem("meetingID") && (
+              <p style={{ fontSize: "12px" }}>
+                *Would be able to add agendas and attachments, once the meeting is saved.
+              </p>
+            )}
+          </div>
+          <div style={{ width: "60%", paddingLeft: 20, position: 'relative' }}>
+            <h3>Attachments</h3>
+            <label for="file-upload" class="custom-file-upload"
+              style={{
+                fontSize: '0.875rem',
+                fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
+                cursor: !localStorage.getItem("meetingID") ? 'default' : 'pointer',
+                border: !localStorage.getItem("meetingID") ? 'none' : '1px solid #1976d2',
+                color: !localStorage.getItem("meetingID") ? 'rgba(0, 0, 0, 0.26)' : '#fff',
+                background: !localStorage.getItem("meetingID") ? 'rgba(0, 0, 0, 0.12)' : '#1976d2',
+                position: 'absolute', padding: !localStorage.getItem("meetingID") ? "10px 16px" : "8.8px 16px",
+                borderRadius: 5
+              }}>
+              UPLOAD
+            </label>
+            <input disabled={!localStorage.getItem("meetingID") ? true : false} id="file-upload" value={currentAttachment} type="file" style={{ display: 'none' }} onChange={getFile} />
+            <Button
+              disabled={getAttachmentState}
+              onClick={saveAttachments}
+              style={{ marginLeft: 100, background: "green" }}
+              variant="contained"
+            >
+              Save
+            </Button>
+
+            <div style={{ width: '100%', marginTop: 10, height: '200px' }}>
+              {attachments.length ?
+                <div style={{ width: '100%', height: '100%', overflowY: 'scroll', padding: 5 }}>
+                  {attachments.map((item) => {
+                    return getFileElement(item)
+                  })}
+                </div> :
+                <p style={{ fontSize: "12px" }}>
+                  No Attachments
+                </p>
+              }
+            </div>
+          </div>
         </div>
-        <div style={{ width: "40%", position: "relative" }}>
+        <div style={{ width: "30%", position: "relative" }}>
           <Button
             onClick={goToAgendas}
             variant="contained"
--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    baseUrl:'http://localhost:3004/'
+    baseUrl:'https://meeting.hiltonpharma.com:3004'
     
 };
 
