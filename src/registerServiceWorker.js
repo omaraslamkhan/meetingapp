0c6ff8635f93e1a86038406e66cc9a715070c825
Muhammad Amanullah Khan <m.amanullahkhan@yahoo.com>
2022-07-16 13:34:36 -0700

    Update registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,3 +1,4 @@
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
@@ -49,13 +50,38 @@ export default function register() {
         registerValidSW(swUrl);
       }
     });
+  } else {
+    console.log("Can't Register to SW. Current env:", process.env.NODE_ENV);
   }
 }
+
+
+window.addEventListener('push', function(event) {
+  console.log('Push Notification received', event);
+
+  var data = {title: 'New!', content: 'Something new happened!'};
+
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+
+  var options = {
+    body: data.content,
+    icon: '/src/icons/logo.png',
+    badge: '/src/icons/logo.png'
+  };
+
+  event.waitUntil(
+    window.registration.showNotification(data.title, options)
+  );
+});
+
 
 function registerValidSW(swUrl) {
   navigator.serviceWorker
     .register(swUrl)
-    .then((registration) => {
+    .then(registration => {
+      console.log("SW is Registered!");
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         installingWorker.onstatechange = () => {
@@ -76,7 +102,7 @@ function registerValidSW(swUrl) {
         };
       };
     })
-    .catch((error) => {
+    .catch(error => {
       console.error("Error during service worker registration:", error);
     });
 }
@@ -84,14 +110,14 @@ function registerValidSW(swUrl) {
 function checkValidServiceWorker(swUrl) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
-    .then((response) => {
+    .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
       if (
         response.status === 404 ||
         response.headers.get("content-type").indexOf("javascript") === -1
       ) {
         // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then((registration) => {
+        navigator.serviceWorker.ready.then(registration => {
           registration.unregister().then(() => {
             window.location.reload();
           });
@@ -110,7 +136,7 @@ function checkValidServiceWorker(swUrl) {
 
 export function unregister() {
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.ready.then((registration) => {
+    navigator.serviceWorker.ready.then(registration => {
       registration.unregister();
     });
   }
